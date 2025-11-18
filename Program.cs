f091e7516059e1a090f899c7ded0411f2e75f8cd
Yoon Lopez <yoon.lopez.7e9@itb.cat>
2025-11-18 11:35:58 +0100

    Finished Chapter 2, closes #3

--- Program.cs
+++ Program.cs
@@ -28,50 +28,46 @@ public class Program
         const string TitleMedium = "You're a Magic Wind Summoner.";
         const string TitleHigh = "Wow! You can summon dragons without burning the lab!";
         const string TitleAdvanced = "You assumed Arcanes Master's rank!";
+        const string EndTraining = "Training complete! {0} has achieved a total power of {1} and earned the title '{2}'";
 
-        const string Dice0Row1 = "              ________ ";
-        const string Dice0Row2 = "             /       /|";
-        const string Dice0Row3 = "            /_______/ |";
+        const string MonsterAppears = "A wild {0} appears! Rolling dice to determine the outcome of the battle...";
+        const string MonsterHPMessage = "The {0} has {1} HP";
+        const string DiceRollMessage = "You rolled a {0}!";
+        const string MonsterTakeDamage = "The monster takes damage!";
+        const string RollAgain = "Press any key to roll the dice again...";
+        const string MonsterDefeated = "The {0} has been defeated!";
+        const string LevelUp = "{0} levels up!";
+        const string MaxLevel = "{0} already reached the max level";
 
-        const string Dice1Row4 = "            |       | |";
-        const string Dice1Row5 = "            |   o   | /";
-        const string Dice1Row6 = "            |       |/";
-
-        const string Dice2Row4 = "            | o     | |";
-        const string Dice2Row5 = "            |       | /";
-        const string Dice2Row6 = "            |     o |/";
-
-        const string Dice3Row4 = "            | o     | |";
-        const string Dice3Row5 = "            |   o   | /";
-        const string Dice3Row6 = "            |     o |/ ";
-
-        const string Dice4Row4 = "            | o   o | |";
-        const string Dice4Row5 = "            |       | /";
-        const string Dice4Row6 = "            | o   o |/ ";
-
-        const string Dice5Row4 = "            | o   o | |";
-        const string Dice5Row5 = "            |   o   | /";
-        const string Dice5Row6 = "            | o   o |/ ";
-
-        const string Dice6Row4 = "            | o   o | |";
-        const string Dice6Row5 = "            | o   o | /";
-        const string Dice6Row6 = "            | o   o |/ ";
-
-
-        const string Dice0Row7 = "            '-------'  ";
-
-        int op;
-        int wizardLevel = 0;
+        int op, actHP, monster, diceResult;
+        int wizardXP = 0;
+        int wizardLevel = 1;
         int hoursTrained = 0;
+        string actMonster;
         string wizardName = "";
         string title = "";
+
+        int[] monsterHP = { 3, 5, 10, 11, 18, 15, 20, 50 };
+        string[] titlesList = { "Raoden the Elantry", "Zyn the Bugged", "Arka Nullpointer", "Elarion of the Embers", "ITB - Wizard the Gray" };
+        string[] monsterNames = { "Wandering Skeleton", "Forest Goblin", "Green Slime", "Ember Wolf", "Giant Spider", "Iron Golem", "Lost Necromancer", "Ancient Dragon"};
+        string[] dice =
+        {
+            "   ________\r\n  /       /|   \r\n /_______/ |\r\n |       | |\r\n |   o   | /\r\n |       |/ \r\n '-------'\r\n",
+            "   ________\r\n  /       /|   \r\n /_______/ |\r\n | o     | |\r\n |       | /\r\n |     o |/ \r\n '-------'\r\n",
+            "   ________\r\n  /       /|   \r\n /_______/ |\r\n | o     | |\r\n |   o   | /\r\n |     o |/ \r\n '-------'\r\n",
+            "   ________\r\n  /       /|   \r\n /_______/ |\r\n | o   o | |\r\n |       | /\r\n | o   o |/ \r\n '-------'\r\n",
+            "   ________\r\n  /       /|   \r\n /_______/ |\r\n | o   o | |\r\n |   o   | /\r\n | o   o |/ \r\n '-------'\r\n",
+            "   ________\r\n  /       /|   \r\n /_______/ |\r\n | o   o | |\r\n | o   o | /\r\n | o   o |/ \r\n '-------'\r\n"
+        };
+        //I do the dice as an array so I can't avoid doing a long condition afterwards, and I can show whatever I need with just one line that works for all the dice sides
+
         Random rnd = new Random();
 
         do {
             Console.WriteLine(MenuTitle);
             if (!string.IsNullOrEmpty(wizardName))
             {
-                Console.WriteLine(MenuWelcome, wizardName, title, wizardLevel);
+                Console.WriteLine(MenuWelcome, wizardName, title, wizardXP);
             }
             Console.WriteLine(MenuOption1);
             Console.WriteLine(MenuOption2);
@@ -115,34 +111,65 @@ public class Program
                     for (int i = 1; i <= meditationDays; i++)
                     {
                         hoursTrained += rnd.Next(1, 25);
-                        wizardLevel += rnd.Next(1, 11);
-                        Console.WriteLine(TrainingOutput, i, wizardName, hoursTrained, wizardLevel);
+                        wizardXP += rnd.Next(1, 11);
+                        Console.WriteLine(TrainingOutput, i, wizardName, hoursTrained, wizardXP);
                     }
 
-                    switch (wizardLevel)
+                    switch (wizardXP)
                     {
-                        case >= 40:
-                            Console.WriteLine(TitleAdvanced);
-                            title = "ITB - Wizard the Gray";
-                            break;
-                        case >= 35:
-                            Console.WriteLine(TitleHigh);
-                            title = "Elarion of the Embers";
-                            break;
-                        case >= 30:
-                            Console.WriteLine(TitleMedium);
-                            title = "Arka Nullpointer";
-                            break;
-                        case >= 20:
-                            Console.WriteLine(TitleBasic);
-                            title = "Zyn the Bugged";
-                            break;
                         case < 20:
+                            title = titlesList[0];
                             Console.WriteLine(TitleFail);
-                            title = "Raoden the Elantry";
                             break;
-                        }
+                        case < 30:
+                            title = titlesList[1];
+                            Console.WriteLine(TitleBasic);
+                            break;
+                        case < 35:
+                            title = titlesList[2];
+                            Console.WriteLine(TitleMedium);
+                            break;
+                        case < 40:
+                            title = titlesList[3];
+                            Console.WriteLine(TitleHigh);
+                            break;
+                        case >= 40:
+                            title = titlesList[4];
+                            Console.WriteLine(TitleAdvanced);
+                            break;
+                    }
+                    Console.WriteLine(EndTraining, wizardName, wizardXP, title);
                     break;
+                case 2:
+                    monster = rnd.Next(0, monsterNames.Length);
+                    actHP = monsterHP[monster];
+                    actMonster = monsterNames[monster];
+                    Console.WriteLine(MonsterAppears, actMonster);
+                    Console.WriteLine(MonsterHPMessage, actMonster, actHP);
+                    do
+                    {
+                        diceResult = rnd.Next(1,7);
+                        Console.WriteLine(DiceRollMessage, diceResult);
+                        Console.WriteLine(dice[diceResult - 1]);
+                        actHP = actHP - diceResult  < 0 ? 0 : actHP - diceResult;
+                        Console.WriteLine(MonsterTakeDamage);
+                        Console.WriteLine(MonsterHPMessage, actMonster, actHP);
+                        if (actHP > 0)
+                        {
+                            Console.WriteLine(RollAgain);
+                            Console.ReadLine();
+                        }
+                    } while (actHP > 0);
+                    Console.WriteLine(MonsterDefeated, actMonster);
+                    if (wizardLevel < 5)
+                    {
+                        Console.WriteLine(LevelUp, wizardName);
+                        wizardLevel++;
+                    } else
+                    {
+                        Console.WriteLine(MaxLevel, wizardName);
+                    }
+                        break;
                 case 0:
                     break;
                 default:
